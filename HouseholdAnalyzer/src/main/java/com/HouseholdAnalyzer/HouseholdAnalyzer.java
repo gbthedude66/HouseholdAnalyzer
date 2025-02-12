23096d6ea4844aa8befb4c139679063b42073ac7
gbthedude66 <garrettbecker425@gmail.com>
2025-02-12 14:03:15 -0800

    refactored, updated pom.xml for building the jar

--- HouseholdAnalyzer/src/main/java/com/HouseholdAnalyzer/HouseholdAnalyzer.java
+++ HouseholdAnalyzer/src/main/java/com/HouseholdAnalyzer/HouseholdAnalyzer.java
@@ -1,3 +1,5 @@
+package com.HouseholdAnalyzer;
+
 import java.io.*;
 import java.util.*;
 
--- HouseholdAnalyzer/src/main/java/com/HouseholdAnalyzer/Person.java
+++ HouseholdAnalyzer/src/main/java/com/HouseholdAnalyzer/Person.java
@@ -1,3 +1,5 @@
+package com.HouseholdAnalyzer;
+
 public class Person {
     private String firstName;
     private String lastName;
